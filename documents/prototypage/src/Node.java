6ce055a331a72345d00ab7f0646504f5481680c0
Louis Leenart <louis.leenar@gmail.com>
2021-01-13 15:18:10 +0100

    ajout fichier node prototypage java

---
+++ documents/prototypage/src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    private int key;
+    private Node lson, rson;
+
+    public Node(int key) {
+        this.key = key;
+        this.lson = null;
+        this.rson = null;
+    }
+}
