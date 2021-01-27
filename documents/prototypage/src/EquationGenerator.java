670db56e94c05715f18bbdde4cea824d4ad48294
Vincent <vincent.commin@etu.univ-poitiers.fr>
2021-01-27 14:45:11 +0100

    Ajout de la méthode de génération

--- documents/prototypage/src/EquationGenerator.java
+++ documents/prototypage/src/EquationGenerator.java
@@ -6,27 +6,15 @@ public abstract class EquationGenerator {
     public static final int         BOUND = 10;                 // Limite des nombres à générer (entre 0 et 9 (BOUND - 1))
 
     private static final int        NB_EXP = 3;                 // Nombre d'expressions pouvant être instancié
-    private static final int        CHANCES_SUM = 100;          // Somme du tableau d'entier "chances"
-    private static int[]            chances = new int[NB_EXP];  // Tableau contenant le % de chance d'instanciation (0 = Plus, 1 = Times, 2 = Number)
+    private static int[]            chances = {50, 80, 100};  // Tableau contenant le % de chance d'instanciation (0 = Plus, 1 = Times, 2 = Number) il doit être de la forme [x, x + y, x + y + z]
     
-
-    // Calcul la somme des cases d'un tableau d'entier
-    private static int tableSum(int[] tab)
-    {
-        int sum = 0;
-        for(int i = 0; i < tab.length; i++)
-            sum += tab[i];
-
-        return sum;
-    }
-
     // Change le tableau de chance d'instanciation si la somme du nouveau tableau est = CHANCE_SUM
     public static void initChances(int[] c) throws Exception
     {
-        if(tableSum(c) == CHANCES_SUM)
+        if(c.length == NB_EXP)
             System.arraycopy(c, 0, chances, 0, c.length);
         else
-            throw new Exception("Table sum not equal to " + CHANCES_SUM);
+            throw new Exception("table length is not valid");
     }
 
     // génère une équation (arbre de type Exp)
@@ -40,8 +28,19 @@ public abstract class EquationGenerator {
         {
             int rand = RAND.nextInt(NB_EXP);
 
-            if(chances)
+            if(rand < chances[0])
+            {
+                return new Plus(generateEquation(hmax - 1), generateEquation(hmax - 1));
+            }
+            else if(rand < chances[1])
+            {
+                return new Times(generateEquation(hmax - 1), generateEquation(hmax - 1));
+            }
+            else
+            {
+                return new Number(RAND.nextInt(BOUND));
+            }
         }
-    }
+     }
 
 }
