729ad64c33095aa93d05e16367693ca7c760fbb2
Florian <florian-1992@hotmail.fr>
2021-02-13 18:49:09 +0100

    Le clavier a maintenant des touches avec des groupes de couleurs

--- gymaths/app/src/main/java/com/example/gymaths/MyKeyboard.java
+++ gymaths/app/src/main/java/com/example/gymaths/MyKeyboard.java
@@ -1,6 +1,7 @@
 package com.example.gymaths;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.SparseArray;
